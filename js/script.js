a13f9c99b4963cc24c947c27f5d7454b1d8276af
dstph <genyadstph@gmail.com>
2020-09-20 11:10:58 +0400

    SOLID

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-class  Greeter{
+class capitalFirstLetter {
 	constructor(guests){
 		this.guest = guests;
 	}
 	greet(){
 		this.guest = this.guest.map(function(x){ return capitalize(x.toUpperCase().toLowerCase()); })					
+	}
+}
+
+
+
+
+class  Greeter extends capitalFirstLetter{
+	constructor(guests){
+		super(guests);
 	}
 	sendGreeting(){
 		this.greet();
@@ -21,6 +30,11 @@ const guests = ['baLOO', 'chARliE', 'BEth'];
 
 let example = new Greeter(guests);
 example.sendGreeting();
+
+
+
+
+
 
 
 
